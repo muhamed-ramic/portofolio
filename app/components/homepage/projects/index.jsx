e42dd94fab8316147c60c80648e9857d9e6f0c20
mramicz <mramic@zerionsoftware.com>
2025-10-11 00:12:18 +0200

    Update projects

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { projectsData } from '@/utils/data/projects-data';
-import ProjectCard from './project-card';
+import SingleProject from './single-project';
 
 const Projects = () => {
 
@@ -26,7 +26,7 @@ const Projects = () => {
               className="sticky-card w-full mx-auto max-w-2xl sticky"
             >
               <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">
-                <ProjectCard project={project} />
+                <SingleProject project={project} />
               </div>
             </div>
           ))}
