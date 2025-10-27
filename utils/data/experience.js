6d40b4e0890009371e9e85a044215ea27d759a2a
mramicz <mramic@zerionsoftware.com>
2025-10-27 19:01:40 +0100

    Update portofolio section

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,18 +3,18 @@ export const experiences = [
     id: 1,
     title: 'Frontend developer"',
     company: "Zerion Software Inc.",
-    duration: "(Jun 2020 - Present)"
+    duration: "(Jun 2020 - Jun 2025)"
   },
   {
     id: 2,
     title: "Frontend developer",
     company: "MTH IT Solutions",
-    duration: "(Oct 2020 - Jun 2020)"
+    duration: "(Oct 2019 - Jun 2020)"
   },
   {
     id: 3,
     title: "Backend developer",
     company: "Cuspis d.o.o",
-    duration: "(Jun 2019 - Jun 2020)"
+    duration: "(Jun 2019 - Oct 2019)"
   }
 ]
